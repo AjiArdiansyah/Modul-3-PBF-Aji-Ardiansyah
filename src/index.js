04f1f9394606bcf81dfd92bdb364f61e75a706ee
AjiArdiansyah <55706264+AjiArdiansyah@users.noreply.github.com>
2022-03-06 16:43:33 +0700

    praktikum

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import Test from './Test';
+import HelloComponent from './component/HelloComponent';
+
+// function HelloComponent(){
+//   return <p>HelloComponent</p>
+// }
+
+// class StateFullComponent extends React.Component {
+//   render() {
+//     return <p>Statefull Component</p>;
+//   }
+// }
+
+// ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+class StateFullComponent extends React.Component { 
+  render() { 
+    return <p>StateFullComponent</p> }
+  }
+
+ReactDOM.render(
+  <HelloComponent />, document.getElementById('root')
+);
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
